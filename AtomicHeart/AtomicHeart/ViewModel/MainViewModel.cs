8c458b36ed746f1d45a2d65fa66c3d08273f3648
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2025-04-17 12:12:21 +0300

    Add view model

---
+++ AtomicHeart/AtomicHeart/ViewModel/MainViewModel.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using System.Collections.Generic;
+
+namespace AtomicHeart
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly Stack<LocationModel> _backStack = new();
+        private readonly Stack<LocationModel> _forwardStack = new();
+        private LocationModel _currentLocation;
+        private int _currentImageIndex = 0;
+
+        public ObservableCollection<LocationModel> Locations { get; set; }
+
+        public LocationModel CurrentLocation
+        {
+            get => _currentLocation;
+            set
+            {
+                if (_currentLocation != value)
+                {
+                    if (_currentLocation != null) _backStack.Push(_currentLocation);
+                    _currentLocation = value;
+                    _currentImageIndex = 0;
+                    OnPropertyChanged();
+                    OnPropertyChanged(nameof(CurrentImage));
+                }
+            }
+        }
+
+        public string CurrentImage =>
+            CurrentLocation?.ImagePaths != null && CurrentLocation.ImagePaths.Count > 0
+                ? CurrentLocation.ImagePaths[_currentImageIndex]
+                : null;
+
+        public ICommand NavigateCommand => new RelayCommand<LocationModel>(location =>
+        {
+            if (location != null)
+            {
+                _forwardStack.Clear();
+                CurrentLocation = location;
+            }
+        });
+
+        public ICommand BackCommand => new RelayCommand(() =>
+        {
+            if (_backStack.Count > 0)
+            {
+                _forwardStack.Push(_currentLocation);
+                _currentLocation = _backStack.Pop();
+                _currentImageIndex = 0;
+                OnPropertyChanged(nameof(CurrentLocation));
+                OnPropertyChanged(nameof(CurrentImage));
+            }
+        });
+
+        public ICommand ForwardCommand => new RelayCommand(() =>
+        {
+            if (_forwardStack.Count > 0)
+            {
+                _backStack.Push(_currentLocation);
+                _currentLocation = _forwardStack.Pop();
+                _currentImageIndex = 0;
+                OnPropertyChanged(nameof(CurrentLocation));
+                OnPropertyChanged(nameof(CurrentImage));
+            }
+        });
+
+        public ICommand NextImageCommand => new RelayCommand(() =>
+        {
+            if (CurrentLocation?.ImagePaths == null) return;
+            if (_currentImageIndex < CurrentLocation.ImagePaths.Count - 1)
+            {
+                _currentImageIndex++;
+                OnPropertyChanged(nameof(CurrentImage));
+            }
+        });
+
+        public ICommand PrevImageCommand => new RelayCommand(() =>
+        {
+            if (CurrentLocation?.ImagePaths == null) return;
+            if (_currentImageIndex > 0)
+            {
+                _currentImageIndex--;
+                OnPropertyChanged(nameof(CurrentImage));
+            }
+        });
+
+        public MainViewModel()
+        {
+            Locations = new ObservableCollection<LocationModel>
+            {
+                new() { 
+                    Name = "Челомей", 
+                    ImagePaths = new List<string> {
+                    "C:\\Users\\User\\source\\repos\\.Net-Technology-Course\\AtomicHeart\\AtomicHeart\\View\\Imgs\\1.jpg",
+                    "C:\\Users\\User\\source\\repos\\.Net-Technology-Course\\AtomicHeart\\AtomicHeart\\View\\Imgs\\2.jpg",
+                    "C:\\Users\\User\\source\\repos\\.Net-Technology-Course\\AtomicHeart\\AtomicHeart\\View\\Imgs\\3.jpg",
+                    "C:\\Users\\User\\source\\repos\\.Net-Technology-Course\\AtomicHeart\\AtomicHeart\\View\\Imgs\\4.png"
+                    }, 
+                    Description = "Город, расположенный на летающей платформе «Икар» и представляющий собой конструкторское бюро машиностроения."
+                },
+                new() {
+                    Name = "Вавилов",
+                    ImagePaths = new List<string> {
+                    "",
+                    ""
+                    },
+                    Description = "Экспериментальный ботанический комплекс."
+                },
+                new() {
+                    Name = "Павлов",
+                    ImagePaths = new List<string> {
+                    "",
+                    ""
+                    },
+                    Description = "Экспериментальный медицинский комплекс."
+                },
+                new() {
+                    Name = "Нептун",
+                    ImagePaths = new List<string> {
+                    "",
+                    ""
+                    },
+                    Description = "В данном комплексе расположена Академия Последствий, а также архив."
+                },
+                new() {
+                    Name = "ВДНХ",
+                    ImagePaths = new List<string> {
+                    "",
+                    ""
+                    },
+                    Description = "Главная выставка Предприятия 3826."
+                },
+                new() {
+                    Name = "Менделеев",
+                    ImagePaths = new List<string> {
+                    "",
+                    ""
+                    },
+                    Description = "Экспериментальный химико-технологический комплекс."
+                },
+                new() {
+                    Name = "Театр имени Майи Плисецкой",
+                    ImagePaths = new List<string> {
+                    "",
+                    ""
+                    },
+                    Description = "Основанный в 1948 году и расположенный на территории Предприятия первый в мире роботизированный театр."
+                },
+                new() {
+                    Name = "Гостиница «Лёгкая»",
+                    ImagePaths = new List<string> {
+                    "",
+                    ""
+                    },
+                    Description = "Уникальный центр отдыха и восстановления, в первую очередь созданный для поддержания сил сотрудников Предприятия 3826, а также простых граждан и интуристов."
+                },
+                new() {
+                    Name = "Тритон",
+                    ImagePaths = new List<string> {
+                    "",
+                    ""
+                    },
+                    Description = ""
+                },
+                new() {
+                    Name = "Лаборатория «Шар»",
+                    ImagePaths = new List<string> {
+                    "",
+                    ""
+                    },
+                    Description = "Мобильная лаборатория профессора Лебедева."
+                },
+                new() {
+                    Name = "Деревня Лесника",
+                    ImagePaths = new List<string> {
+                    "",
+                    ""
+                    },
+                    Description = "Населённый пункт, расположенный на территории комплекса «Вавилов», который используется для обслуживания нужд комплекса, в том числе для обеспечения жильём части его персонала."
+                },
+            };
+            CurrentLocation = Locations[0];
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
