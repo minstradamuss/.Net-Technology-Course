2413b3e73415951afe386bc558df946570c9f554
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2025-04-17 12:11:17 +0300

    Add model

---
+++ AtomicHeart/AtomicHeart/Model/LocationModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AtomicHeart
+{
+    public class LocationModel
+    {
+        public string Name { get; set; }
+        public List<string> ImagePaths { get; set; }
+        public string Description { get; set; }
+    }
+}
