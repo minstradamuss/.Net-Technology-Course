86ee64080db2d26691ae431531cea125911c99ae
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2025-05-08 23:52:44 +0300

    Add

---
+++ Project/ChatBook/Program.cs
@@ -0,0 +1,61 @@
+﻿using ChatBook.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Windows.Forms;
+using ChatBook.DataAccess;
+using ChatBook.Migrations;
+using ChatBook.UI.Forms;
+using ChatBook.UI.Windows;
+
+namespace ChatBook
+{
+    internal static class Program
+    {
+        public static IServiceProvider ServiceProvider { get; private set; }
+
+        [STAThread]
+        static void Main()
+        {
+            ApplyMigrations();
+            ConfigureServices();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Получаем сервис
+            var loginForm = ServiceProvider.GetRequiredService<LoginForm>();
+            Application.Run(loginForm);
+        }
+
+        static void ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<ApplicationDbContext>();
+            services.AddSingleton<UserService>();
+
+            // Регистрируем формы
+            services.AddTransient<LoginForm>();
+            services.AddTransient<MainForm>();
+            services.AddTransient<FriendsForm>();
+            services.AddTransient<EditProfileWindow>();
+            services.AddTransient<ChatForm>();
+            services.AddTransient<BookSearchWindow>();
+            services.AddTransient<AddBookWindow>();
+
+            ServiceProvider = services.BuildServiceProvider();
+
+            // инициализация БД
+            Database.SetInitializer(new DB.DbInitializer());
+        }
+
+        static void ApplyMigrations()
+        {
+            var configuration = new Configuration();
+            var migrator = new DbMigrator(configuration);
+            migrator.Update();
+        }
+    }
+}
---
+++ Project/ChatBook/UI/Forms/FriendsForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ChatBook.UI.Forms
+{
+    partial class FriendsForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelFriends;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnShowFriends;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanelFriends = new System.Windows.Forms.FlowLayoutPanel();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnShowFriends = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanelFriends
+            // 
+            this.flowLayoutPanelFriends.AutoScroll = true;
+            this.flowLayoutPanelFriends.BackColor = System.Drawing.Color.Moccasin;
+            this.flowLayoutPanelFriends.Location = new System.Drawing.Point(20, 60);
+            this.flowLayoutPanelFriends.Name = "flowLayoutPanelFriends";
+            this.flowLayoutPanelFriends.Size = new System.Drawing.Size(500, 300);
+            this.flowLayoutPanelFriends.TabIndex = 0;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(20, 20);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(200, 22);
+            this.txtSearch.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.Moccasin;
+            this.btnSearch.Location = new System.Drawing.Point(230, 18);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(80, 26);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Поиск";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnShowFriends
+            // 
+            this.btnShowFriends.BackColor = System.Drawing.Color.Moccasin;
+            this.btnShowFriends.Location = new System.Drawing.Point(320, 18);
+            this.btnShowFriends.Name = "btnShowFriends";
+            this.btnShowFriends.Size = new System.Drawing.Size(120, 26);
+            this.btnShowFriends.TabIndex = 3;
+            this.btnShowFriends.Text = "Мои подписки";
+            this.btnShowFriends.UseVisualStyleBackColor = false;
+            this.btnShowFriends.Click += new System.EventHandler(this.btnShowFriends_Click);
+            // 
+            // FriendsForm
+            // 
+            this.BackColor = System.Drawing.Color.OldLace;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.flowLayoutPanelFriends);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnShowFriends);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(618, 447);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(618, 447);
+            this.Name = "FriendsForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Друзья";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
